45c65034805522761cf9419f7dacf8f0a32fe838
Patino5 <ryan.patino5@gmail.com>
2025-08-07 12:55:55 -0500

    renamed TravelappApplication to BucketApplication

--- src/test/java/com/travelbucket/travelapp/BucketApplicationTests.java
+++ src/test/java/com/travelbucket/travelapp/BucketApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TravelappApplicationTests {
+class BucketApplicationTests {
 
 	@Test
 	void contextLoads() {
