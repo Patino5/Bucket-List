06d615a93084c268c8b77961086d3906673ba8d5
Patino5 <ryan.patino5@gmail.com>
2025-08-07 12:46:56 -0500

    Initial Spring Setup

---
+++ src/test/java/com/travelbucket/travelapp/TravelappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbucket.travelapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
