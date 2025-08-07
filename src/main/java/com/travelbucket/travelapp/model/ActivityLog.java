952565d4f40b584c2fbfed0101e73a1f5dabfb8c
Patino5 <ryan.patino5@gmail.com>
2025-08-07 14:58:06 -0500

    ActivityLog class added using Lombok

---
+++ src/main/java/com/travelbucket/travelapp/model/ActivityLog.java
@@ -0,0 +1,19 @@
+package com.travelbucket.travelapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ActivityLog {
+    private int memoryID;
+    private int activityID;
+    private LocalDateTime createdAt;
+    private String notes;
+    private String photoURL;
+
+}
