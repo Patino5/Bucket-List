493deb843e3c78aedb97d9c2e911d8e0c8f025ee
Patino5 <ryan.patino5@gmail.com>
2025-08-12 09:30:28 -0500

    command line runner imported and impl

--- src/main/java/com/travelbucket/travelapp/BucketApplication.java
+++ src/main/java/com/travelbucket/travelapp/BucketApplication.java
@@ -1,13 +1,11 @@
 package com.travelbucket.travelapp;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BucketApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BucketApplication.class, args);
-	}
-
+public class BucketApplication implements CommandLineRunner {
+	public static void main(String[] args) {SpringApplication.run(BucketApplication.class, args);}
+	public void run(String... args) throws Exception {}
 }
