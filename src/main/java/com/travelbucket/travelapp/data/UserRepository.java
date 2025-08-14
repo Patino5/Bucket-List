5cf718d80e1636a0c746f0c490349ca9ac01ff71
Patino5 <ryan.patino5@gmail.com>
2025-08-14 09:44:57 -0500

    find by username and password added method added;

--- src/main/java/com/travelbucket/travelapp/data/UserRepository.java
+++ src/main/java/com/travelbucket/travelapp/data/UserRepository.java
@@ -7,6 +7,7 @@ import java.util.List;
 public interface UserRepository {
     public List<User> findAll();
     public User findById(int userID);
+    public User findByUsernameAndPassword(String userName, String userPassword);
     public User add(User user);
     public User updateUser(User user);
     public boolean deleteUser(int userID);
