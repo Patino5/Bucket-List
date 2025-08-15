92e74f2d57787443fa9947baf9af99b00daef5f4
Patino5 <ryan.patino5@gmail.com>
2025-08-15 16:26:01 -0500

    activityLogRepo implemented from activityRepository interface

---
+++ src/main/java/com/travelbucket/travelapp/data/impl/ActivityLogRepo.java
@@ -0,0 +1,84 @@
+package com.travelbucket.travelapp.data.impl;
+
+import com.travelbucket.travelapp.data.ActivityLogRepository;
+import com.travelbucket.travelapp.data.mappers.ActivityLogRowMapper;
+import com.travelbucket.travelapp.model.ActivityLog;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class ActivityLogRepo implements ActivityLogRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    ActivityLogRowMapper activityLogRowMapper;
+
+    @Override
+    public List<ActivityLog> findAll() {
+        String sql = "SELECT * FROM ActivityLog";
+        return jdbcTemplate.query(sql, activityLogRowMapper);
+    }
+
+    @Override
+    public ActivityLog findById(int memoryID) {
+        String sql = "SELECT * FROM ActivityLog Where memoryID = ?;";
+        return jdbcTemplate.queryForObject(sql, activityLogRowMapper, memoryID);
+    }
+
+    @Override
+    public ActivityLog add(ActivityLog activityLog) {
+        String sql = "INSERT INTO ActivityLog (activityID, createdAt, notes, photoURL) VALUES (? ,?, ?, ?);";
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, activityLog.getActivityID());
+
+            if (activityLog.getCreatedAt() != null) {
+                ps.setTimestamp(2, Timestamp.valueOf(activityLog.getCreatedAt()));
+            } else {
+                ps.setTimestamp(2, Timestamp.valueOf(LocalDateTime.now()));
+            }
+
+            ps.setString(3, activityLog.getNotes());
+            ps.setString(4, activityLog.getPhotoURL());
+
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        activityLog.setMemoryID(keyHolder.getKey().intValue());
+        return activityLog;
+    }
+
+    @Override
+    public ActivityLog update(ActivityLog activityLog) {
+        String sql = "UPDATE ActivityLog SET notes = ?, photoURL = ?, WHERE memoryID = ?";
+
+        int rows = jdbcTemplate.update(sql,
+                activityLog.getNotes(),
+                activityLog.getPhotoURL(),
+                activityLog.getMemoryID()
+        );
+
+        return rows > 0 ? activityLog : null;
+    }
+
+    @Override
+    public boolean delete(int memoryID) {
+        String sql = "DELETE FROM ActivityLog WHERE memoryID = ?;";
+        return jdbcTemplate.update(sql, memoryID) > 0;
+    }
+}
