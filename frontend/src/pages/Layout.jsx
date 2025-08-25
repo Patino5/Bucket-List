e552a9e62b38d419794dfedff1fb3577e92be122
Patino5 <ryan.patino5@gmail.com>
2025-08-25 09:37:08 -0500

    hide all  nav buttons on login screen

--- frontend/src/pages/Layout.jsx
+++ frontend/src/pages/Layout.jsx
@@ -8,8 +8,6 @@ const Layout = () => {
         // clear saved user data
         localStorage.removeItem("userID");
         localStorage.removeItem("userName");
-
-        navigate("/layout/login"); // redirect after logout
     };
 
     return (
@@ -18,25 +16,31 @@ const Layout = () => {
                 <h1 className="text-5xl">Travel Bucket</h1>
                 <nav>
                     <ul className="flex gap-4">
-                        <li>
-                            <button className="border-1 py-1 px-2 rounded-4xl text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700"><Link to="/layout/home">Home</Link></button>
-                        </li>
-                        <li>
-                            <button className="border-1 py-1 px-2 rounded-4xl text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700"><Link to="/layout/destinations">AddDestination</Link></button>
-                        </li>
-                        <li>
-                            <button className="border-1 py-1 px-2 rounded-4xl text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700"><Link to="/layout/activitylog">ActivityLog</Link></button>
-                        </li>
+
                         {/* Hide Logout on login page */}
                         {location.pathname !== "/layout/login" && (
-                            <li>
-                                <button
-                                    onClick={handleLogout}
-                                    className="border-1 py-1 px-2 rounded-4xl text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 cursor-pointer"
-                                >
-                                    Logout
-                                </button>
-                            </li>
+                            <>
+                                <li>
+                                    <Link className="border-1 py-2 px-3 rounded-4xl text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700" to="/layout/home">
+                                        Destinations
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link className="border-1 py-2 px-3 rounded-4xl text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700" to="/layout/activitylog">
+                                        Memories
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link
+                                        onClick={handleLogout}
+                                        className="border-1 py-2 px-3 rounded-4xl text-purple-600 hover:border-transparent hover:bg-purple-600 hover:text-white active:bg-purple-700 cursor-pointer"
+                                        to="/layout/login"
+                                    >
+                                        Logout
+                                    </Link>
+                                </li>
+                            </>
+
                         )}
                     </ul>
                 </nav>
