6ee56f2876a6f0fec7918660309a0ce16ab6e9ec
Patino5 <ryan.patino5@gmail.com>
2025-08-25 09:38:50 -0500

    removed comments and styling changes

--- frontend/src/pages/DestinationDetails.jsx
+++ frontend/src/pages/DestinationDetails.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router";
-import { getActivities, deleteDestination, deleteActivity } from "./api"; // you'll need to implement deleteDestination & deleteActivity
+import { getActivities, deleteDestination, deleteActivity } from "./api";
+import Loading from "../components/Loading";
 
 const DestinationDetails = ({ destinations, setDestinations }) => {
     const { id } = useParams();
@@ -36,7 +37,7 @@ const DestinationDetails = ({ destinations, setDestinations }) => {
             });
     }, [id, destination]);
 
-    if (loading) return <p>Loading activities...</p>;
+    if (loading) return <Loading />
     if (error) return <p className="text-red-500">{error}</p>;
     if (!destination) return <p>Destination not found</p>;
 
@@ -58,21 +59,25 @@ const DestinationDetails = ({ destinations, setDestinations }) => {
         navigate(`/layout/home/destination/${id}/add-activity`);
     };
 
+    const openLogForm = (activityID) => {
+        navigate(`/layout/home/destination/${id}/activity/${activityID}/add-memory`)
+    }
+
     const handleDeleteActivity = async (activityID) => {
         try {
             await deleteActivity(activityID);
             setActivities((prev) => prev.filter((a) => a.activityID !== activityID));
-            setActivityToDelete(null); // close modal
+            setActivityToDelete(null);
         } catch (err) {
             alert("Error deleting activity: " + err.message);
         }
     };
 
     return (
-        <div className="p-6">
+        <div>
             {/* City background */}
             <div
-                className="p-6 bg-cover bg-center"
+                className="px-6 py-20 bg-cover bg-center"
                 style={{ backgroundImage: `url(${destination.imageUrl})` }}
             >
                 <div className="shadow-2xl w-sm p-5 rounded-4xl text-neutral-100 bg-black/60">
@@ -91,63 +96,73 @@ const DestinationDetails = ({ destinations, setDestinations }) => {
                             ? new Date(destination.destinationDeparture).toLocaleString()
                             : "N/A"}
                     </p>
-                    <div className="mt-5 flex gap-4">
+                </div>
+            </div>
+            <div className="py-5 flex gap-4 justify-center bg-neutral-300">
                         <button
                             onClick={handleEditDestination}
-                            className="bg-sky-500 hover:bg-sky-700 py-2 px-3 rounded-full"
+                            className="bg-sky-500 hover:bg-sky-700 py-3 px-4 rounded-md"
                         >
                             Edit Destination
                         </button>
                         <button
                             onClick={() => setShowDeleteDestModal(true)}
-                            className="bg-red-600 hover:bg-red-800 py-2 px-3 rounded-full"
+                            className="bg-red-600 hover:bg-red-800 py-3 px-4 rounded-md"
                         >
                             Delete Destination
                         </button>
                         <button
                             onClick={handleAddActivity}
-                            className="bg-amber-500 hover:bg-amber-700 py-2 px-3 rounded-full"
+                            className="bg-amber-500 hover:bg-amber-700 py-3 px-4 rounded-md"
                         >
                             Add Activity
                         </button>
                     </div>
-                </div>
-            </div>
 
             {/* Activities */}
-            <div className="md:grid md:grid-cols-2 md:gap-5">
+            <div className="md:grid md:grid-cols-2 md:gap-5 mx-6">
                 {activities.length > 0 ? (
                     activities.map((a) => (
                         <div
                             key={a.activityID}
-                            className="space-y-2 bg-neutral-200 shadow-xl text-center rounded py-7 my-10"
+                            className="space-y-2 bg-neutral-200 shadow-xl text-center rounded p-7 my-10"
                         >
-                            <div className="space-y-0.5">
-                                <p className="text-lg font-semibold text-black">{a.title}</p>
+                            <div className="space-y-2">
+                                <p className="text-2xl font-semibold text-black">{a.title}</p>
                                 <p>{a.activityDescription}</p>
                                 <p>
                                     Website:{" "}
-                                    <a href={a.website} target="_blank" rel="noreferrer">
+                                    <a className="font-light" href={a.website} target="_blank" rel="noreferrer">
                                         {a.website}
                                     </a>
                                 </p>
                             </div>
+                            {a.isCompleted ? (
+                                <button className="bg-blue-600 px-4 py-3 rounded-md text-xs text-neutral-200 cursor-pointer" onClick={() => openLogForm(a.activityID)}>
+                                    ADD MEMORY
+                                </button>
+                            ) : (
+                                <span className="text-gray-500 italic mt-5">
+                                    Complete this activity to add a log
+                                </span>
+                            )}
                             <div className="mt-5 flex justify-evenly ">
                                 <button
                                     onClick={() =>
                                         navigate(`/layout/home/destination/${id}/activity/${a.activityID}/edit`)
                                     }
-                                    className="bg-purple-500 py-1 px-2 h-8 w-35 rounded-full text-xs text-neutral-200 cursor-pointer"
+                                    className="bg-purple-500 py-1 px-2 h-8 w-35 rounded-md text-xs text-neutral-200 cursor-pointer"
                                 >
                                     EDIT ACTIVITY
                                 </button>
                                 <button
                                     onClick={() => setActivityToDelete(a)}
-                                    className="bg-red-500 py-1 px-2 h-8 w-35 rounded-full text-xs text-neutral-200 cursor-pointer"
+                                    className="bg-red-500 py-1 px-2 h-8 w-35 rounded-md text-xs text-neutral-200 cursor-pointer"
                                 >
                                     DELETE ACTIVITY
                                 </button>
                             </div>
+                        
                         </div>
                     ))
                 ) : (
