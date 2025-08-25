5df0ab6fb1822e01e7ba15aa9633a0b4a631d7ed
Patino5 <ryan.patino5@gmail.com>
2025-08-25 16:35:02 -0500

    activityList added to pull out from destinationdetails

---
+++ frontend/src/components/ActivityList.jsx
@@ -0,0 +1,25 @@
+import ActivityCard from "./ActivityCard";
+
+const ActivityList = ({ activities, id, openLogForm, onEdit, onDelete }) => {
+    
+    return (
+        <div className="max-w-6xl mx-auto px-6 py-10 grid md:grid-cols-2 gap-8">
+            {activities.length > 0 ? (
+                activities.map((a) => (
+                    <ActivityCard
+                        key={a.activityID}
+                        activity={a}
+                        id={id}
+                        openLogForm={openLogForm}
+                        onEdit={onEdit}
+                        onDelete={onDelete}
+                    />
+                ))
+            ) : (
+                <p className="text-center text-gray-600 italic">Add an activity to your destination!</p>
+            )}
+        </div>
+    );
+}
+
+export default ActivityList;
